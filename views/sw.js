ab34239da58a10bf10546114a906bcec44e3de18
Jasper Anthony W. Zhang <2160563@slu.edu.ph>
2019-07-08 15:35:19 +0800

    sdfdsf

--- views/sw.js
+++ views/sw.js
@@ -8,7 +8,24 @@ if ("serviceWorker" in navigator) {
 const CACHE_NAME = "cache-v1";
 const assetToCache = [
   "/",
-    "/home"
+    "/home",
+    "/assets/css/bootstrap.css",
+    "/assets/css/bootstrap-responsive.css",
+    "/assets/css/docs.css",
+    "/assets/css/prettyPhoto.css",
+    "/assets/js/google-code-prettify/prettify.css",
+    "/assets/css/nivo-slider.css",
+    "/assets/css/style.css",
+    "/assets/css/overwrite.css",
+    "/assets/css/boxtile.css",
+    "/assets/color/success.css",
+    "/assets/img/slides/nivo/3.png",
+    "/assets/img/slides/nivo/4.png",
+    "/assets/img/slides/nivo/5.png",
+    "/assets/img/slides/nivo/6.png",
+    "/assets/img/slides/nivo/7.png",
+    "/assets/img/slides/nivo/1.png",
+    "/assets/img/slides/nivo/2.png"
 ];
 self.addEventListener("install", function(event) {
   console.log("installing");
