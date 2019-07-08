62a100cbff2874e1efe9331f1a05f42eee558bd0
Jasper Anthony W. Zhang <2160563@slu.edu.ph>
2019-07-08 15:40:12 +0800

    sdfdsgf

--- public/sw.js
+++ public/sw.js
@@ -8,24 +8,7 @@ if ("serviceWorker" in navigator) {
 const CACHE_NAME = "cache-v1";
 const assetToCache = [
   "/",
-    "/home",
-    "/assets/css/bootstrap.css",
-    "/assets/css/bootstrap-responsive.css",
-    "/assets/css/docs.css",
-    "/assets/css/prettyPhoto.css",
-    "/assets/js/google-code-prettify/prettify.css",
-    "/assets/css/nivo-slider.css",
-    "/assets/css/style.css",
-    "/assets/css/overwrite.css",
-    "/assets/css/boxtile.css",
-    "/assets/color/success.css",
-    "/assets/img/slides/nivo/3.png",
-    "/assets/img/slides/nivo/4.png",
-    "/assets/img/slides/nivo/5.png",
-    "/assets/img/slides/nivo/6.png",
-    "/assets/img/slides/nivo/7.png",
-    "/assets/img/slides/nivo/1.png",
-    "/assets/img/slides/nivo/2.png"
+    "/home"
 ];
 self.addEventListener("install", function(event) {
   console.log("installing");
@@ -38,8 +21,26 @@ self.addEventListener("install", function(event) {
       .catch(console.error)
   );
 });
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
 });
 self.addEventListener('push', event => {
   const data = event.data.json();
